20bcc99056b6ceaa5548c5e911e88494893ed64e
Bogdan <49450616+Bogdan26Bogdan@users.noreply.github.com>
2025-01-23 17:58:17 -0800

    make sure to pull before committing and pushing

--- bash_cron.sh
+++ bash_cron.sh
@@ -25,6 +25,7 @@ time_diff=$((current_time - last_mod_time))
 
 # Check if the difference is greater than or equal to 3600 seconds (1 hour)
 if [ $time_diff -ge 3600 ]; then
+    git pull
     git commit -m "cron job" -m "grabbed api data at $(TZ="America/Vancouver" date)"
     git push
     echo "Pushed to git at $(TZ="America/Vancouver" date)" >> $SCRIPTPATH/cron.log
