af38f50e3dfecebe5f29e832f55bbd6611bdad94
Bogdan <49450616+Bogdan26Bogdan@users.noreply.github.com>
2025-01-23 18:14:37 -0800

    chaange when cd

--- bash_cron.sh
+++ bash_cron.sh
@@ -2,12 +2,12 @@
 
 # Get the path to the script
 SCRIPTPATH=$(dirname "$0")
+cd $SCRIPTPATH
 
 # Run the script to query the api
 /usr/bin/python3 $SCRIPTPATH/Ferry_api_cron.py
 
-# Add to git and push 
-cd $SCRIPTPATH
+# Add to git 
 git add .
 
 # Get the current time in seconds since epoch
@@ -23,7 +23,7 @@ fi
 # Calculate the difference in time
 time_diff=$((current_time - last_mod_time))
 
-# Check if the difference is greater than or equal to 3600 seconds (1 hour)
+# Check if the difference is greater than or equal to 3600 seconds (1 hour) and if it is push to git
 if [ $time_diff -ge 3600 ]; then
     git pull
     git commit -m "cron job" -m "grabbed api data at $(TZ="America/Vancouver" date)"
